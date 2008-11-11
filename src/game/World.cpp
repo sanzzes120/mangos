9ab1c749ba6a96b0e6cc224ccf09917b33e323c6
dythzer <micke223@gmail.com>
2008-11-11 19:48:04 +0100

    Made impconfig compatible with arena patch and possible other patches too
    Note: Configuration options moved to top of mangosd.conf

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -800,50 +800,6 @@ void World::LoadConfigSettings(bool reload)
     m_configs[CONFIG_LISTEN_RANGE_TEXTEMOTE] = sConfig.GetIntDefault("ListenRange.TextEmote", 25);
     m_configs[CONFIG_LISTEN_RANGE_YELL]      = sConfig.GetIntDefault("ListenRange.Yell", 300);
 
-    //CUSTOM OPTIONS START - ImpConfig
-	m_PlayerStartGold = sConfig.GetFloatDefault("PlayerStart.Gold", 0);
-	if(m_PlayerStartGold < 0)
-		m_PlayerStartGold = 0;
-
-	if(m_PlayerStartGold > MAX_MONEY_AMOUNT)
-		m_PlayerStartGold = MAX_MONEY_AMOUNT;
-
-	m_configs[CONFIG_PLAYER_START_HONOR] = sConfig.GetIntDefault("PlayerStart.HonorPoints", 0);
-	if(m_configs[CONFIG_PLAYER_START_HONOR] < 0)
-		m_configs[CONFIG_PLAYER_START_HONOR] = 0;
-
-	m_configs[CONFIG_PLAYER_START_ARENAPTS] = sConfig.GetIntDefault("PlayerStart.ArenaPoints", 0);
-	if(m_configs[CONFIG_PLAYER_START_ARENAPTS] < 0)
-		m_configs[CONFIG_PLAYER_START_ARENAPTS] = 0;
-
-	m_configs[CONFIG_GM_START_LEVEL] = sConfig.GetIntDefault("GamemasterStartLevel", 70);
-	if(m_configs[CONFIG_GM_START_LEVEL] < 1)
-		m_configs[CONFIG_GM_START_LEVEL] = 1;
-
-	m_configs[CONFIG_INSTANT_LOGOUT] = sConfig.GetBoolDefault("PlayerInstantLogout", false);
-	m_configs[CONFIG_BG_START_MUSIC] = sConfig.GetBoolDefault("MusicInBattleground", false);
-	m_configs[CONFIG_START_ALL_SPELLS] = sConfig.GetBoolDefault("PlayerStart.AllSpells", false);
-	m_configs[CONFIG_QUEUE_FOR_GM] = sConfig.GetBoolDefault("EnableQueueForGMs", false);
-	m_configs[CONFIG_HONOR_AFTER_DUEL] = sConfig.GetIntDefault("HonorPointsAfterDuel", 0);
-	if(m_configs[CONFIG_HONOR_AFTER_DUEL] < 0)
-		m_configs[CONFIG_HONOR_AFTER_DUEL]= 0;
-	m_configs[CONFIG_KICK_FROM_GMISLAND] = sConfig.GetBoolDefault("AntiCheat.GMIsland", false);
-	m_configs[CONFIG_START_ALL_EXPLORED] = sConfig.GetBoolDefault("PlayerStart.MapsExplored", false);
-	m_configs[CONFIG_DISABLE_BREATHING] = sConfig.GetBoolDefault("DisableWaterBreath", false);
-	m_configs[CONFIG_DISABLE_RES_SICKNESS] = sConfig.GetBoolDefault("DisableResurrectSickness", false);
-	m_configs[CONFIG_START_ALL_REP] = sConfig.GetBoolDefault("PlayerStart.AllReputation", false);
-	m_configs[CONFIG_ALWAYS_MAXSKILL] = sConfig.GetBoolDefault("AlwaysMaxWeaponSkill", false);
-	m_configs[CONFIG_START_ALL_TAXI] = sConfig.GetBoolDefault("PlayerStart.AllFlightPaths", false);
-	m_configs[CONFIG_PVP_TOKEN_ENABLE] = sConfig.GetBoolDefault("PvPToken.Enable", false);
-	m_configs[CONFIG_PVP_TOKEN_MAP_TYPE] = sConfig.GetIntDefault("PvPToken.MapAllowType", 4);
-	m_configs[CONFIG_PVP_TOKEN_ID] = sConfig.GetIntDefault("PvPToken.ItemID", 29434);
-	m_configs[CONFIG_PVP_TOKEN_COUNT] = sConfig.GetIntDefault("PvPToken.ItemCount", 1);
-	if(m_configs[CONFIG_PVP_TOKEN_COUNT] < 1)
-		m_configs[CONFIG_PVP_TOKEN_COUNT] = 1;
-	m_configs[CONFIG_NO_RESET_TALENT_COST] = sConfig.GetBoolDefault("NoResetTalentsCost", false);
-    m_configs[CONFIG_DEFAULT_GUILD_ID] = sConfig.GetIntDefault("PlayerStart.DefaultGuildId", 0);
-	//CUSTOM OPTIONS END - ImpConfig
-
     m_VisibleUnitGreyDistance = sConfig.GetFloatDefault("Visibility.Distance.Grey.Unit", 1);
     if(m_VisibleUnitGreyDistance >  MAX_VISIBILITY_DISTANCE)
     {
@@ -896,6 +852,50 @@ void World::LoadConfigSettings(bool reload)
         sLog.outError("Visibility.Distance.InFlight can't be greater %f",MAX_VISIBILITY_DISTANCE-m_VisibleObjectGreyDistance);
         m_MaxVisibleDistanceInFlight = MAX_VISIBILITY_DISTANCE - m_VisibleObjectGreyDistance;
     }
+
+	//CUSTOM OPTIONS START - ImpConfig
+	m_PlayerStartGold = sConfig.GetFloatDefault("PlayerStart.Gold", 0);
+	if(m_PlayerStartGold < 0)
+		m_PlayerStartGold = 0;
+
+	if(m_PlayerStartGold > MAX_MONEY_AMOUNT)
+		m_PlayerStartGold = MAX_MONEY_AMOUNT;
+
+	m_configs[CONFIG_PLAYER_START_HONOR] = sConfig.GetIntDefault("PlayerStart.HonorPoints", 0);
+	if(m_configs[CONFIG_PLAYER_START_HONOR] < 0)
+		m_configs[CONFIG_PLAYER_START_HONOR] = 0;
+
+	m_configs[CONFIG_PLAYER_START_ARENAPTS] = sConfig.GetIntDefault("PlayerStart.ArenaPoints", 0);
+	if(m_configs[CONFIG_PLAYER_START_ARENAPTS] < 0)
+		m_configs[CONFIG_PLAYER_START_ARENAPTS] = 0;
+
+	m_configs[CONFIG_GM_START_LEVEL] = sConfig.GetIntDefault("GamemasterStartLevel", 70);
+	if(m_configs[CONFIG_GM_START_LEVEL] < 1)
+		m_configs[CONFIG_GM_START_LEVEL] = 1;
+
+	m_configs[CONFIG_INSTANT_LOGOUT] = sConfig.GetBoolDefault("PlayerInstantLogout", false);
+	m_configs[CONFIG_BG_START_MUSIC] = sConfig.GetBoolDefault("MusicInBattleground", false);
+	m_configs[CONFIG_START_ALL_SPELLS] = sConfig.GetBoolDefault("PlayerStart.AllSpells", false);
+	m_configs[CONFIG_QUEUE_FOR_GM] = sConfig.GetBoolDefault("EnableQueueForGMs", false);
+	m_configs[CONFIG_HONOR_AFTER_DUEL] = sConfig.GetIntDefault("HonorPointsAfterDuel", 0);
+	if(m_configs[CONFIG_HONOR_AFTER_DUEL] < 0)
+		m_configs[CONFIG_HONOR_AFTER_DUEL]= 0;
+	m_configs[CONFIG_KICK_FROM_GMISLAND] = sConfig.GetBoolDefault("AntiCheat.GMIsland", false);
+	m_configs[CONFIG_START_ALL_EXPLORED] = sConfig.GetBoolDefault("PlayerStart.MapsExplored", false);
+	m_configs[CONFIG_DISABLE_BREATHING] = sConfig.GetBoolDefault("DisableWaterBreath", false);
+	m_configs[CONFIG_DISABLE_RES_SICKNESS] = sConfig.GetBoolDefault("DisableResurrectSickness", false);
+	m_configs[CONFIG_START_ALL_REP] = sConfig.GetBoolDefault("PlayerStart.AllReputation", false);
+	m_configs[CONFIG_ALWAYS_MAXSKILL] = sConfig.GetBoolDefault("AlwaysMaxWeaponSkill", false);
+	m_configs[CONFIG_START_ALL_TAXI] = sConfig.GetBoolDefault("PlayerStart.AllFlightPaths", false);
+	m_configs[CONFIG_PVP_TOKEN_ENABLE] = sConfig.GetBoolDefault("PvPToken.Enable", false);
+	m_configs[CONFIG_PVP_TOKEN_MAP_TYPE] = sConfig.GetIntDefault("PvPToken.MapAllowType", 4);
+	m_configs[CONFIG_PVP_TOKEN_ID] = sConfig.GetIntDefault("PvPToken.ItemID", 29434);
+	m_configs[CONFIG_PVP_TOKEN_COUNT] = sConfig.GetIntDefault("PvPToken.ItemCount", 1);
+	if(m_configs[CONFIG_PVP_TOKEN_COUNT] < 1)
+		m_configs[CONFIG_PVP_TOKEN_COUNT] = 1;
+	m_configs[CONFIG_NO_RESET_TALENT_COST] = sConfig.GetBoolDefault("NoResetTalentsCost", false);
+    m_configs[CONFIG_DEFAULT_GUILD_ID] = sConfig.GetIntDefault("PlayerStart.DefaultGuildId", 0);
+	//CUSTOM OPTIONS END - ImpConfig
 
     ///- Read the "Data" directory from the config file
     std::string dataPath = sConfig.GetStringDefault("DataDir","./");
